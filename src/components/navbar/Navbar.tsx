2a665623322e65a3b2e088759e28595f68b7fc91
paula_leao <paularegina396@gmail.com>
2026-08-18 09:01:08 -0300

    :sparkles: Routes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,25 @@
+import { Link } from "react-router-dom"
+ 
 function Navbar() {
-
-  return (
-    <>
-    {/* // Container principal */}
-    <div className="bg-indigo-900 w-full flex justify-center py-4 text-white"
-    >
-        {/* Div do titulo */}
-        <div className="container flex justify-between text-lg mx-8"
-        >
-            Blog Pessoal
-            {/* Div da NavBar mesmo */}
-            <div className="flex gap-4"
-            >
-                Postagens
-                Temas
-                Cadastrar tema
-                Perfil
-                Sair
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+                            bg-indigo-900 text-white'>
+           
+                <div className="container flex justify-between text-lg mx-8">
+                    <Link to="/" className="text-2xl font-bold">Blog Pessoal </Link>
+ 
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                       <Link to="/login" className="hover:underline">Sair </Link>
+                    </div>
+                </div>
             </div>
-        </div>
-        
-
-    </div>
-    </>
-  )
+        </>
+    )
 }
-
+ 
 export default Navbar
