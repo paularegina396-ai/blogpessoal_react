53067a6940ccad542cb41f370d9c13c6b8ef31f4
paula_leao <paularegina396@gmail.com>
2026-08-21 07:53:13 -0300

    :sparkles: feat: tema

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -38,4 +38,31 @@ export const cadastrarUsuario = async (url: string, dados:Object, setDados: Func
 export const login = async (url: string, dados:Object, setDados: Function) =>{
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
+
+    
+}
+
+// COM TOKEN - Função Consultar com token
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header);
+    setDados(resposta.data);
+}
+
+// Função Cadastrar com token
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+
+// Função Atualizar com token
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+
+// Função deletar com token
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header);
 }
